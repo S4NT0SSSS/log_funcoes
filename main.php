090f6e628f4636b778055f761ea50e864e08ac84
santos <joao.v.santos46@edu.senai.br>
2026-04-23 12:03:30 -0300

    v.0.4 - adicionado função de multiplicação ao projeto

--- main.php
+++ main.php
@@ -9,11 +9,12 @@ function soma($a, $b, $c){
 function Subtrair($a, $b){
     return($a - $b);
 }
-echo "resultado: ", Subtrair(10,5);
 
+function Multiplicar($a, $b){
+    return($a * $b);
+}
 
-
-
+echo "resultado: ", Multiplicar(10,5);
 
 
 
